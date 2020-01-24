b0e5ef56b507c0daae531d33434a6406cc1b17aa
Dmitry Groza <boxfrommars@gmail.com>
2020-01-24 19:01:39 +0300

    Parse data from wikipedia

---
+++ app/Console/Commands/ParseStats.php
@@ -0,0 +1,85 @@
+<?php
+
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use Throwable;
+use Illuminate\Support\Facades\Log;
+
+
+class ParseStats extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'parse:stats';
+
+    protected $description = 'Parse Wikipedia data about infected people';
+
+    /**
+     * Execute the console command.
+     * @throws Throwable
+     */
+    public function handle()
+    {
+        # action=parse&page=2019–20_Wuhan_coronavirus_outbreak&prop=wikitext&section=0&format=json&noimages=true
+        $endPoint = "https://en.wikipedia.org/w/api.php";
+        $params = [
+            "action" => "parse",
+            "page" => "2019–20_Wuhan_coronavirus_outbreak",
+            "format" => "json",
+            "prop" => "wikitext",
+            "section" => 0,
+        ];
+
+        $url = $endPoint . "?" . http_build_query($params);
+
+        $ch = curl_init( $url );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        $output = curl_exec( $ch );
+        curl_close( $ch );
+
+        $result = json_decode($output, true);
+
+        $resultLines = explode("\n\n", $result["parse"]["wikitext"]["*"] );
+
+        foreach($resultLines as $line) {
+            if (strpos($line, "'''Total'''") > 0) {
+                preg_match_all('/\'\'\'[^\']+\'\'\'/', $line, $matches, PREG_OFFSET_CAPTURE);
+
+                if (count($matches) === 1) {
+                    $matches = $matches[0];
+
+                    if (count($matches) === 3) {
+                        Log::debug('OK!');
+
+                        $infected = str_replace('\'', '', $matches[1][0]);
+                        $dead = str_replace('\'', '', $matches[2][0]);
+
+                        $result = [
+                            'infected' => (int) $infected,
+                            'dead' => (int) $dead,
+                        ];
+
+                        file_put_contents(storage_path('app' . DIRECTORY_SEPARATOR . 'data.json'), json_encode($result));
+
+                    } else {
+                        Log::debug('NOT OK!');
+                        Log::debug($line);
+                    }
+
+                } else {
+                    Log::debug('NOT OK!');
+                    Log::debug($line);
+                }
+            }
+
+        }
+
+        Log::debug('Hola');
+    }
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1" />
+    <title>2019-nCoV</title>
+</head>
+<body>
+
+</body>
+</html>

b5a030e3383bbb8121604b4de9ac157b838fd479
Dmitry Groza <boxfrommars@gmail.com>
2020-01-25 14:45:10 +0300

    Style controls

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,10 +32,9 @@
     </video>
 </div>
 
-<div class="container h-100" style="position: relative;">
+<div class="container h-100">
     <h1 class="text-hide page-title" style="">Coronavirus live</h1>
-    <a href="#" id="toggle-audio"
-       style="width: 50px; height: 40px; background-image: url('/video/audio-on.svg'); position: absolute; right:20px; top: 20px;"></a>
+    <a href="#" id="toggle-audio"></a>
 
     <div class="row align-items-center counter-row" style="height: 75%">
         <div class="col text-center">
@@ -59,13 +58,6 @@
 <audio src="/video/bg-audio.mp3" autoplay loop id="bg-audio">
     audio is not supported.
 </audio>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-{{--<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>--}}
-{{--<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>--}}
-{{--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>--}}
-
 
 <script>
     let audio = document.getElementById("bg-audio");

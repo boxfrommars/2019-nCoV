f391237db663469b40ab90b6beba9413e20b5300
Dmitry Groza <boxfrommars@gmail.com>
2020-02-02 13:18:55 +0300

    Recovered

--- app/Console/Commands/ParseStats.php
+++ app/Console/Commands/ParseStats.php
@@ -57,7 +57,7 @@ class ParseStats extends Command
             $result = json_encode([
                 'infected' => (int)$matches[0],
                 'deaths' => (int)$matches[1],
-                'recovered' => 245,
+                'recovered' => 334,
                 'countries' => 18,
             ]);
 
